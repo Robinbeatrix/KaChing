96a63bffcbca59614af6ecd1f18bbb5f1c2a614b
Ben Burk <benburk@outlook.com>
2020-01-12 10:04:07 -0500

    update everything

---
+++ frontend/app/views.py
@@ -0,0 +1,176 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Python modules
+import os, logging 
+
+# Flask modules
+from flask               import render_template, request, url_for, redirect, send_from_directory
+from flask_login         import login_user, logout_user, current_user, login_required
+from werkzeug.exceptions import HTTPException, NotFound, abort
+
+# App modules
+from app        import app, lm, db, bc
+from app.models import User
+from app.forms  import LoginForm, RegisterForm
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
+
+
+
+# provide login manager with load_user callback
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Logout user
+@app.route('/logout.html')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+#Google Cred
+GOOGLE_CLIENT_ID = "570396819834-rcidgre3ooopk63og3ieldsmte9e2mu0.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "16otddj1sWcoIy8qVvCEQ7SR"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+#Oauth Client
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+#Google Config
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+
+
+# Register a new user
+@app.route('/register.html')
+def register():
+    form = RegisterForm(request.form)
+
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/register.html/callback")
+def callback():
+    form = RegisterForm(request.form)
+
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(
+    token_endpoint,
+    authorization_response=request.url,
+    redirect_url=request.base_url,
+    code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+        pw_hash = 'password'
+        user = User(users_name,users_email,pw_hash)
+        user.save()
+        msg = 'User Created, please <a href="' +url_for('login') + '">login</a>'
+
+        return render_template('layouts/auth-default.html',
+                                content=render_template('pages/register.html',
+                                    form = form, msg=msg))
+    else:
+        return "User email not available or not verified by Google.", 400
+
+
+# Authenticate user
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    
+    # Declare the login form
+    form = LoginForm(request.form)
+
+    # Flask message injected into the page, in case of any errors
+    msg = None
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        if user:
+            
+            #if bc.check_password_hash(user.password, password):
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                msg = "Wrong password. Please try again."
+        else:
+            msg = "Unkkown user"
+
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/login.html', form=form, msg=msg ) )
+
+# App main route + generic routing
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path>')
+def index(path):
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    content = None
+
+    try:
+
+        # try to match the pages defined in -> pages/<input file>
+        return render_template('layouts/default.html',
+                                content=render_template( 'pages/'+path) )
+    except:
+        
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/404.html' ) )
+
+# Return sitemap 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
+
+
+
+
